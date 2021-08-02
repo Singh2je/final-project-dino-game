57124aa33bf095099a6f81c2634fa350830f7406
sekhonsj <sekhonsj@mail.uc.edu>
2021-08-02 19:11:58 -0400

    added images

--- main.js
+++ main.js
@@ -1,16 +1,42 @@
 // Create our 'main' state that will contain the game
 var mainState = {
-    preload: function() { },
+    preload: function() {
+        game.load.image('ground', 'assets/ground.png')
+        game.load.image('dino', 'assets/dino.png'); 
+     },
     
     create: function() { 
+        const height = 600;
+        const width = 800;
+
         // Change the background color of the game to blue
-        game.stage.backgroundColor = '#000000';
+        game.stage.backgroundColor = '#808080';
+
+        this.gameSpeed = 10;
+
+        this.ground = this.add.tileSprite(0, height, width, 26, 'ground');
+        this.dino = game.add.sprite(0, height, 'dino');
     
         // Set the physics system
         game.physics.startSystem(Phaser.Physics.ARCADE);
+    
+        // Add physics to the bird
+        // Needed for: movements, gravity, collisions, etc.
+        game.physics.arcade.enable(this.dino);
+    
+        // Add gravity to the bird to make it fall
+        this.dino.body.gravity.y = 1000;  
+    
+        // Call the 'jump' function when the spacekey is hit
+        var spaceKey = game.input.keyboard.addKey(
+                        Phaser.Keyboard.SPACEBAR);
+        spaceKey.onDown.add(this.jump, this);     
+
     },
     
-    update: function() { },
+    update: function() {
+        this.ground.tilePositionX += this.gameSpeed;
+     },
 
     jump: function() { },
     
@@ -21,8 +47,8 @@ var mainState = {
     } 
 };
 
-// Initialize Phaser, and create a 400px by 490px game
-var game = new Phaser.Game(600, 600);
+// Initialize Phaser, and create a 600px by 600px game
+var game = new Phaser.Game(800, 600);
 
 // Add the 'mainState' and call it 'main'
 game.state.add('main', mainState); 
